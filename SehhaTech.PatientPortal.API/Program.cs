04f8fb75ab798b0a7f2808a4323a27159c7b59f2
abdelrahmanKhalawy <abdovectort@gmail.com>
2026-06-21 00:52:48 +0300

    Switch database provider from SQL Server to PostgreSQL in Program.cs

--- SehhaTech.PatientPortal.API/Program.cs
+++ SehhaTech.PatientPortal.API/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Database
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Portal Services
 builder.Services.AddScoped<OtpService>();
